5f7cc77621f1c5b3beb4fcd443a6950ebf340589
Budhanks <eduperezferia@gmail.com>
2025-04-20 18:03:26 -0600

    haciendo pagina

--- proyecto_final/frontend/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const response = await fetch('http://localhost:3001/api/docentes');
-    const docentes = await response.json();
-    const contenedor = document.getElementById('docentes');
-    
-    docentes.forEach(docente => {
-      contenedor.innerHTML += `<p>${docente.nombre} - ${docente.categoria}</p>`;
-    });
-  });
